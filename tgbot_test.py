bb7b2791366f1b78d912e77473155220a4a8362f
kra53n <greasha46@gmail.com>
2026-05-26 20:09:44 +0300

    beautify summary #12

--- tgbot_test.py
+++ tgbot_test.py
@@ -19,3 +19,14 @@ def test_storage():
 
     storage[user_telegram_id].last_input_nicknames = args3
     assert storage[user_telegram_id].last_input_nicknames == args3
+
+
+def test_format_bytes():
+    assert tgbot.format_bytes(0) == "0 bytes"
+    assert tgbot.format_bytes(125) == "125 bytes"
+
+    assert tgbot.format_bytes(1023) == "1023 bytes"
+    assert tgbot.format_bytes(1024) == "1 KiB"
+
+    assert tgbot.format_bytes(126_976) == "124 KiB"
+    assert tgbot.format_bytes(130_000) == "126 KiB"
